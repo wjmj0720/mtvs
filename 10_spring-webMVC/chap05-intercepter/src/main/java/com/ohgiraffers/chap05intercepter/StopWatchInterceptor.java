543dda0a173c1bdd53f7bd072738e43225a473e4
wjmj0720 <wjmj0720@gmail.com>
2025-04-18 01:00:48 +0900

    spring

--- 10_spring-webMVC/chap05-intercepter/src/main/java/com/ohgiraffers/chap05intercepter/StopWatchInterceptor.java
+++ 10_spring-webMVC/chap05-intercepter/src/main/java/com/ohgiraffers/chap05intercepter/StopWatchInterceptor.java
@@ -1,16 +1,46 @@
 package com.ohgiraffers.chap05intercepter;
 
+import jakarta.annotation.Nullable;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class StopWatchInterceptor implements HandlerInterceptor {
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    private final MenuService menuService;
 
-
-
+    public StopWatchInterceptor(MenuService menuService) {
+        this.menuService = menuService;
     }
 
+    /* 전처리 메서드 */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("prehandler 호출함...");
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+
+        return true;
+    }
+
+    /*후처리 메서드*/
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,@Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("post henadler");
+        long startTime = (long) request.getAttribute("startTime");
+        request.removeAttribute("startTime");
+
+        long endTime = System.currentTimeMillis();
+        modelAndView.addObject("interval", endTime-startTime);
+
+    }
+    /* 마지막에 호출하는 메서드 */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+        System.out.println("after completion 호출함");
+        menuService.method();
+    }
 }
